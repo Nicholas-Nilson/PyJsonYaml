8fd2c3a37b9f8ddf37d1f362fe5acf9d07f56ce2
Nick Nilson <nilson.nicholas@gmail.com>
2022-07-13 10:46:43 -0400

    json to yaml now working properly

--- first_pipeline.py
+++ first_pipeline.py
@@ -1,10 +1,3 @@
-import yaml, json
-
-# def json_to_yaml(file_name):
-#     with open(file_name+'.json', 'r') as json_in, open(file_name+'.yaml', 'w') as yaml_out:
-#         json_payload = json.load(json_in)
-#         yaml.dump(json_payload, sort_keys=False)
-
 import json
 import yaml
 
@@ -30,6 +23,3 @@ def do_json_to_yaml(filename):
 if __name__ == '__main__':
         filename = "emojis.json"
         do_json_to_yaml(filename)
-
-# json_to_yaml('donuts')
-json_to_yaml('emojis')
