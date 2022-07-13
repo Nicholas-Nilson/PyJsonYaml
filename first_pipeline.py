154c97244f706ce56c5aeb2bf1f6c55e26e75648
Nick Nilson <nilson.nicholas@gmail.com>
2022-07-13 12:18:45 -0400

    yaml to json now working

--- first_pipeline.py
+++ first_pipeline.py
@@ -1,25 +1,53 @@
 import json
 import yaml
+import sys
+
+
+def is_yaml(filename):
+    return '.yaml' in filename
+
+
+def do_yaml_to_json(filename):
+    print('do yaml to json')
+    f = open(filename)
+    data = yaml.safe_load(f.read())
+    temp_data = json.dumps(data)
+    output_filename = filename.replace('.yaml', '.json')
+    output_file = open(output_filename, 'w')
+    output_file.write(temp_data)
+
+    f.close()
+    output_file.close()
+
 
 
 def do_json_to_yaml(filename):
-        pass
-        print('do json to yaml')
-        f = open(filename)
-        # step 2
-        data = json.loads(f.read())
-        ## print(type(data), data)
+    pass
+    print('do json to yaml')
+    f = open(filename)
+    # step 2
+    data = json.loads(f.read())
+    ## print(type(data), data)
 
-        # step 3
-        ## converts to a yaml string
-        tdata = yaml.dump(data)
-        output_filename = filename.replace('.json', '.yaml')
-        output_file = open(output_filename, "w")
-        output_file.write(tdata)
-        # cleanup
-        f.close()
-        output_file.close()
+    # step 3
+    ## converts to a yaml string
+    tdata = yaml.dump(data)
+    output_filename = filename.replace('.json', '.yaml')
+    output_file = open(output_filename, "w")
+    output_file.write(tdata)
+    # cleanup
+    f.close()
+    output_file.close()
+
+
 
 if __name__ == '__main__':
-        filename = "emojis.json"
+    # filename = "xmas.yaml"
+    if len(sys.argv) != 2:
+        print("you forgot the filename!")
+        exit()
+    filename = sys.argv[1]
+    if is_yaml(filename):
+        do_yaml_to_json(filename)
+    else:
         do_json_to_yaml(filename)
--- y2j.py
+++ y2j.py
@@ -1,11 +1,25 @@
 import yaml, json
 
-def yaml_to_json(file_name):
-    with open(file_name+'.yaml', 'r') as file:
-        configuration = yaml.safe_load(file_name)
 
-    with open(file_name+'.json', 'w') as json_file:
-        json.dump(configuration, json_file)
+def do_yaml_to_json(filename):
+    print('do yaml to json')
+    f = open(filename)
+    data = yaml.safe_load(f.read())
+    temp_data = json.dumps(data)
+    output_filename = filename.replace('.yaml', '.json')
+    output_file = open(output_filename, 'w')
+    output_file.write(temp_data)
+
+    f.close()
+    output_file.close()
+
+
+# def yaml_to_json(file_name):
+#     with open(file_name+'.yaml', 'r') as file:
+#         configuration = yaml.safe_load(file_name)
+#
+#     with open(file_name+'.json', 'w') as json_file:
+#         json.dump(configuration, json_file)
 
 # yaml_to_json'verify')
 yaml_to_json('xmas')
